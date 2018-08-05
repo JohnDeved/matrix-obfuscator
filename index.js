998dc3356e449775f91a29212c757d5506f0389d
undefined <18speedytrue@gmail.com>
2018-08-05 04:44:21 +0200

    + fist push

---
+++ index.js
@@ -0,0 +1,73 @@
+const crypto = require('crypto')
+
+class MatrixLogic {
+  get count () {
+    if (typeof this._count === typeof 0) {
+      if (this._count >= this._pass.length - 1) {
+        this._count = 0
+      } else {
+        this._count++
+      }
+    } else {
+      this._count = 0
+    }
+    return this._count
+  }
+
+  _processor (x, decode) {
+    if (this._pass) {
+      let key = this._pass[this.count]
+      this._key = key.charCodeAt(0)
+    }
+    let d = x
+    if (this._key) {
+      if (!decode) {
+        d += (this._key + 30000)
+      } else {
+        d -= (this._key + 30000)
+      }
+    }
+    console.log(x, String.fromCharCode(x), d)
+    return String.fromCharCode(d)
+  }
+
+  _toUnicodeArray (str) {
+    let arr = str.split('')
+    return arr.map(x => x.charCodeAt(0))
+  }
+
+  init (input, pass, decode) {
+    if (typeof pass === typeof '') {
+      this._pass = crypto.createHmac('sha256', pass)
+        .digest('hex')
+    }
+
+    if (typeof input === typeof '') {
+      if (decode) {
+        input = input.split(' ').join('')
+      }
+      let array = this._toUnicodeArray(input)
+
+      return array
+        .map(x => this._processor(x, decode))
+        .join(decode ? '' : ' ')
+    }
+  }
+}
+
+class MatrixObfuscator {
+  static _logic (input, pass, decode) {
+    return new MatrixLogic()
+      .init(input, pass, decode)
+  }
+
+  static encode (input, pass) {
+    return this._logic(input, pass, false)
+  }
+
+  static decode (input, pass) {
+    return this._logic(input, pass, true)
+  }
+}
+
+module.exports = MatrixObfuscator
---
+++ test.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const matrixObfuscator = require('./index')
+
+let test = `
+class StaticMethodCall{
+  constructor(){
+      console.log(StaticMethodCall.staticMethod()); 
+      // 'static method has been called' 
+      
+      console.log(this.constructor.staticMethod()); 
+      // 'static method has been called' 
+  }
+
+  static  staticMethod(){
+      return 'static method has been called.';
+  }
+}
+`
+test = `Hello`
+
+let encode = matrixObfuscator.encode(test, 'secret password')
+fs.writeFileSync('./test.emoji', encode)
+
+let decode = matrixObfuscator.decode(encode, 'secret password')
+fs.writeFileSync('./test.text', decode)
+
+console.info('done')
